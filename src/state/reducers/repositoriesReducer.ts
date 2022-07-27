a14ec16abdba0d70c716858880a393a328281d3b
yahya <ahmadyahya1800@gmail.com>
2022-07-27 22:58:57 +0100

    Repos Reducer Setup

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -4,6 +4,31 @@ interface RepositoriesState {
   data: string[];
 }
 
-const reducer = (state: RepositoriesState, action: any) => {};
+const reducer = (state: RepositoriesState, action: any) => {
+  switch (action.type) {
+    case "search_repositories":
+      return {
+        loading: true,
+        error: null,
+        data: [],
+      };
+    case "search_repositories_success":
+      return {
+        loading: false,
+        error: null,
+        data: action.payload,
+      };
+
+    case "search_repositories_error":
+      return {
+        loading: false,
+        error: action.payload,
+        data: [],
+      };
+
+    default:
+      return state;
+  }
+};
 
 export default reducer;
