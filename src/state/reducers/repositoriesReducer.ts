6c3e13c10c400c104894c884670d6f7a4dfd2761
yahya <ahmadyahya1800@gmail.com>
2022-07-27 22:47:14 +0100

    initial setup

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,9 @@
+interface RepositoriesState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+const reducer = (state: RepositoriesState, action: any) => {};
+
+export default reducer;
