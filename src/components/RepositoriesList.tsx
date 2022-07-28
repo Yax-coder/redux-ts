b782caa8cc061811db52c78a0b9be1a665d57654
yahya <ahmadyahya1800@gmail.com>
2022-07-28 19:00:00 +0100

    Selecting state using the useSelector hook

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { useState } from "react";
 import { useActions } from "../hooks/useActions";
+import { useSelector } from "react-redux";
 
 const RepositoriesList = () => {
   const [term, setTerm] = useState("");
   const { searchRepositories } = useActions();
+  const { data, error, loading } = useSelector((state) => state.repositories);
+
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
