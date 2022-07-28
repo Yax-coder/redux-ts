900f5729d5686e761f429875a7ca620069c0bbd2
yahya <ahmadyahya1800@gmail.com>
2022-07-28 14:00:47 +0100

    connecting redux to react

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -7,8 +7,14 @@ interface RepositoriesState {
   data: string[];
 }
 
+const initialState = {
+  loading: false,
+  error: null,
+  data: [],
+};
+
 const repositoriesReducer = (
-  state: RepositoriesState,
+  state: RepositoriesState = initialState,
   action: Action
 ): RepositoriesState => {
   switch (action.type) {
