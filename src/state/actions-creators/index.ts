e1e5c3177103f1ffdb3c82ce17788d15627e1dd5
yahya <ahmadyahya1800@gmail.com>
2022-07-28 18:33:15 +0100

    Binding Action Creators

--- src/state/actions-creators/index.ts
+++ src/state/actions-creators/index.ts
@@ -10,7 +10,7 @@ export const searchRepositories = (term: string) => {
     });
     try {
       const { data } = await axios.get(
-        "https://registry.npmjs.org/-/v1/searh",
+        "https://registry.npmjs.org/-/v1/search",
         {
           params: {
             text: term,
